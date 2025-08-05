8fcff72a008383da630657e460cb903f54e5a2a3
Vinyl <38420904+VinylTheDuster@users.noreply.github.com>
2025-08-05 21:16:15 +0200

    Intialization

---
+++ src/pages/scripts/gallerie.js
@@ -0,0 +1,36 @@
+// get the images in the gallery
+let imgs = document.getElementsByClassName("image-gallery");
+
+// get the modal
+let modal = document.getElementById("modal");
+
+// get the span element that closes the modal
+let span = document.getElementById("close");
+
+// get the image block inside the modal
+let imgBlock = document.getElementById("imgblock");
+
+// when load, set the modal to not display
+window.onload = (event) => {
+    modal.style.display = "none";
+}
+
+// for with function to set the modal images
+for (let i = 0; i < imgs.length; i++) {
+    imgs[i].onclick = function() {
+    imgBlock.src = this.src;
+    modal.style.display = "flex";
+    }
+}
+
+// when the user clicks on the span element
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// when the user clicks outside the modal
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
