99fe95bb738b4614a4861ffcfbd6d007a3099ccd
Vinyl <38420904+VinylTheDuster@users.noreply.github.com>
2025-08-14 20:46:37 +0200

    Improved responsiveness of the “Projets” page, moved nav.js, optimized project-fetcher.js

--- public/scripts/project-fetcher.js
+++ public/scripts/project-fetcher.js
@@ -15,30 +15,35 @@ async function initializeModule(){
 
     // in the event of any error on the part of Supabase
     if (error) {
-        console.error('Erreur Supabase :', error.message);
+        alert('Erreur Supabase :', error.message)
     }
 
     if (!projects || !projects.length) {
-        console.warn(" Aucun projet trouvé dans Supabase")
+        alert('Aucun contenu trouvé dans Supabase')
     }
 
     // loop in the rows of the “projects” table
     for (const project of projects) {
 
-        // stock each column of the “project” element
-        const image = project.image;
-        const name = project.name;
-        const description = project.description;
-        const link = project.link;
-        const ongoing = project.ongoing;
+        // const ongoing = project.ongoing; => to assign later
+
+        // to store a possible placeholder
+        let image = "";
+
+        // check if project.image contain nothing
+        if (!project.image) {
+            image = 'https://sivtvabtcdjkbwygrxjk.supabase.co/storage/v1/object/sign/projects-thumbnails/placeholderthumbnail.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV8yZmM5ZDcyMy0zYWFlLTQzMmUtOGExOC1kOWYwMzZiNDc2ZGIiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJwcm9qZWN0cy10aHVtYm5haWxzL3BsYWNlaG9sZGVydGh1bWJuYWlsLnBuZyIsImlhdCI6MTc1NTE5MTYwNSwiZXhwIjoxNzU1NjIzNjA1fQ.ymQ2pTRb0AfDRDyPyZce3fnj8UPiWC---UXBzErifnM';
+        } else {
+            image = project.image;
+        }
 
         // includes a div class="project-item" in projectGrid with the appropriate content for all rows in the “projects” table
         projectGrid.innerHTML +=
         `<div class="project-item">
             <img src="${image}" alt="">
-			<h2>${name}</h2>
-			<p>${description}</p>
-			<a href="${link}" target="_blank">Lien vers le projet</a>
+			<h2>${project.name}</h2>
+			<p>${project.description}</p>
+			<a href="${project.link}" target="_blank" rel="noopener noreferrer">Lien vers le projet</a>
         </div>`
     };
 };
